57346982c07c9e385be2dd9691d736245e748fac
Sekrafi Abdallah <abdallah.sek@gmail.com>
2023-10-12 14:14:53 +0100

    Update Movies.js

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,30 +1,23 @@
 export const DefautlMovies = [
     {
-        title: "The Creator",
-        description: "Décrit comme un thriller post-apocalyptique impliquant un futur marqué par une guerre entre les humains et l'IA",
-        posterURL: "https://m.media-amazon.com/images/M/MV5BNDUyNTIzNDQtYTZmMi00M2FlLTgyZjUtYWViZWNhMDYzMjE4XkEyXkFqcGdeQXVyMTUzMTg2ODkz._V1_FMjpg_UY2048_.jpg",
-        trailerlink:"https://imdb-video.media-imdb.com/vi2123613977/1434659607842-pgv4ql-1689598037263.mp4?Expires=1696931208&Signature=KJuHCw~Y7pUUQA-VBkWuCg3LrSTav4EYGBKl6fzrbWapNiVH8W7viriooU1NgpVvq3Jn7WAH-whKkv7H5OC73vFYogMWm3XE9ez7iqq3PxFhHVaHDgjH-8lLzaYMrMiWOBC9LBrVcmOohPFkGS9nZF~ZDvFM3Lo-p~n6fo8QPqeAnlAJ2BYWa-SAdmpSf-7rlS0gofl5uKBZMkjku9lHrYh-FGWw~nRr7WU3ZC8MgiHCg8ubKT5EzeSg4K4t1-4zcKUdxiXloV7C4UkMb3nC6nwFaZQaInw7lRN6ANbOSoN0GI5DRthK8oXUWNaBFZL13DAAZ8npkXS3Ye1LfVG4kQ__&Key-Pair-Id=APKAIFLZBVQZ24NQH3KA",
-        rating: "4"
+        title: "Apocalypto",
+        description: "Un jeune homme est embarqué dans un voyage périlleux vers un monde régi par la peur et l'oppression",
+        posterURL: "https://m.media-amazon.com/images/M/MV5BMzhmNGMzMDMtZDM0Yi00MmVmLWExYjAtZDhjZjcxZDM0MzJhXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_FMjpg_UX1200_.jpg",
+        trailerlink:"https://www.youtube.com/embed/ngWBddVNVZs?si=WZ3gondwsEE2jNnu",
+        rating: "5"
     },
     {
-        title: "No One Will Save You",
-        description: "Une femme au foyer, exilée et angoissée, doit lutter contre un extraterrestre qui s'est introduit dans sa maison",
-        posterURL: "https://m.media-amazon.com/images/M/MV5BMGNhZDczNTUtOWEzZS00ZjEyLTkzODQtOGM1MTZiMGY2ODIyXkEyXkFqcGdeQXVyMjkwOTAyMDU@._V1_FMjpg_UY3000_.jpg",
-        trailerlink:"https://imdb-video.media-imdb.com/vi1239598873/1434659607842-pgv4ql-1693932888326.mp4?Expires=1696931522&Signature=tBkjdM~CKSA35-pusHZbidqyP-m39qydCZbl9IcO1QNzSWHZdYG3H99hFw3-WKCt-dVeINdQ8D5i8cnz6mCYIgU3W1CR-kZKXpvPLlzLXVHER1mkgCFwJvbwnPHZRFjjbFfHmG6pv5uHc5xDyQw22DftpQR7fodvJomuRjzdWJTrKAVZ1oM~K4tH8KjfkkA1gSjiSU6lVnbrrPZr2pAW3DM~zWtYR9Kl5sGrSFAvXmtdQ1FU1d5-OcBPXyeZZdncaHNHJCG4RGw-HzGTV9ZoTWECYF~PSg6F-X3z6IlNWSPUT6eqFEQyMXOtf1eH79WMf08~ZlTnGRRQ89EezmfWmw__&Key-Pair-Id=APKAIFLZBVQZ24NQH3KA",
-        rating: "3"
-    },
-    {
-        title: "Jumanji",
+        title: "Jumanji: Welocome to the jungle",
         description: "Quatre adolescents sont aspirés dans un jeu vidéo magique, et la seule façon de s'échapper est de travailler ensemble",
         posterURL: "https://m.media-amazon.com/images/M/MV5BYzBmMTBmOWQtMTg3Zi00Mzk4LTk3OTMtYmU2NWMwYmJkNTU1XkEyXkFqcGdeQXVyMTEyOTQ1OTk4._V1_FMjpg_UX1200_.jpg",
-        trailerlink:"https://imdb-video.media-imdb.com/vi1725348121/1434659607842-pgv4ql-1498741523486.mp4?Expires=1696931719&Signature=t9WVOZGpS7~55WFuIGyTNroSAQ4AXCphvx3Y7JbfjRp~5hH7BPbGtIEC~ntjKjLWau2oNUM-nvBa~O-L1~xOLo-gCiso09Hr6-JK4Ho3QEVVY9HLY7dQDkVtH4lFkjrBhhUj2MeKjyp~gtqKjnFYdNFxla5NwB8Vgf~sjR7LvZiyXRZkzFFwWX2wxtQTJZN3EgTjxo-JqTqvYwryyzW~BAseK-yAQvXevA5IRr29bNM3fL3TyR8vrNGOJkLYgs6Nc-5Olaq37h3B9MWVC2xcY~PvbByDcvv9LdA5J5Ll2ieHl3zsDC3Sirfdnl3CXFPmFCXY8wc-pfZMrrKnoB9ARg__&Key-Pair-Id=APKAIFLZBVQZ24NQH3KA",
-        rating: "5"
+        trailerlink:"https://www.youtube.com/embed/2QKg5SZ_35I?si=nZO7F9uKYoVPQ9Nc",
+        rating: "3"
     },
     {
         title: "Raya and the Last Dragon",
         description: "Au royaume de Lumandra sur une Terre réinventée, une guerrière nommée Raya est déterminée à trouver le dernier dragon.",
         posterURL: "https://upload.wikimedia.org/wikipedia/en/e/ea/Raya_and_the_Last_Dragon.png",
-        trailerlink:"https://video.internetvideoarchive.net/video.mp4?cmd=6&fmt=4&customerid=442155&publishedid=210139&e=1697529994&videokbrate=1500&h=be1a8133c68a903b087e1191e685a3d3",
+        trailerlink:"https://www.youtube.com/embed/9BPMTr-NS9s?si=AitmPzbue_Wd7Q5Z",
         rating: "4.5"
     },
 ];
